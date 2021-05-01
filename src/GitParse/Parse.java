a8658ddf55c11ce9b0da91d64439bfccfcc633b6
Golfstorm <anre_anre@mail.ru>
2021-05-02 02:01:28 +0300

    02.05.21
    Распарсил остаток лога (Tail)

--- src/GitParse/Parse.java
+++ src/GitParse/Parse.java
@@ -4,50 +4,96 @@ public class Parse
 {	
 	public static void parse(String cut) 
 	{
-		cut.length();				// этот метод тебе возвращает число, но ты его не используешь
-		cut.indexOf("Process");		// аналогично
-		String clearCut = cut.substring(cut.indexOf("Process"), cut.indexOf("AMQ"));
-		clearCut = clearCut.replaceAll("\\s+", "");
-		String tempCut = "";
-		String hmName = "";
-		String hmValue = "";
-		int tempLenght = 0;
-		int startIndex = 0;
-		int stopIndex = 0;
-		while (clearCut.length() != 0) 
+		Data.str_all = cut;
+		String clearCutHead = "";
+		String clearCutTail = "";
+		Data.str_head = Data.str_all.substring(Data.str_all.indexOf("Process"), Data.str_all.indexOf("AMQ"));
+		Data.str_tail = Data.str_all.substring(Data.str_all.indexOf("AMQ"), Data.str_all.indexOf("-----"));
+		clearCutHead = Data.str_head.replaceAll("\\s+", "");
+		clearCutTail = Data.str_tail.replaceAll("\\s+", "");
+		String tempCut;
+		String hmName;
+		String hmValue;
+		int tempLenght;
+		int startIndex;
+		int stopIndex;
+		while (clearCutHead.length() != 0) 
 		{
-			clearCut.length();				// опять
-			if (clearCut.length() != 0) 
-			{
-				startIndex = clearCut.indexOf("(");
-				tempCut = clearCut.substring(0, startIndex);
-				//Data.DataList.add("Name " + tempCut);
+				tempCut = "";
+				hmName = "";
+				hmValue = "";
+				tempLenght = 0;
+				startIndex = 0;
+				stopIndex = 0;
+				startIndex = clearCutHead.indexOf("(");
+				tempCut = clearCutHead.substring(0, startIndex);
 				hmName = tempCut;
 				tempLenght = tempCut.length() +1;
-				clearCut = clearCut.substring(tempLenght, clearCut.length());
+				clearCutHead = clearCutHead.substring(tempLenght, clearCutHead.length());
 				tempCut = "";
-				stopIndex = clearCut.indexOf(")");
-				if (clearCut.indexOf("(") < clearCut.indexOf(")")) 
+				stopIndex = clearCutHead.indexOf(")");
+				if (clearCutHead.indexOf("(") < clearCutHead.indexOf(")")) 
 				{
-					if (clearCut.indexOf("(") == -1) 
+					if (clearCutHead.indexOf("(") == -1) 
 					{
-						stopIndex = clearCut.indexOf(")");
+						stopIndex = clearCutHead.indexOf(")");
 					}
 					else
-					stopIndex = clearCut.indexOf("))") +1;
+					stopIndex = clearCutHead.indexOf("))") +1;
 				}
-				tempCut = clearCut.substring(0, stopIndex);
-				//Data.DataList.add("Value " + tempCut);
+				tempCut = clearCutHead.substring(0, stopIndex);
 				hmValue = tempCut;
 				tempLenght = tempCut.length() +1;
-				if (tempLenght >= clearCut.length()) 
+				if (tempLenght >= clearCutHead.length()) 
 				{
-					tempLenght = clearCut.length();
+					tempLenght = clearCutHead.length();
 				}
-				clearCut = clearCut.substring(tempLenght, clearCut.length());
+				clearCutHead = clearCutHead.substring(tempLenght, clearCutHead.length());
 				tempCut = "";
 				Data.dataList.put(hmName, hmValue);
-			}
+		}
+		while (clearCutTail.length() != 0)
+		{
+				tempCut = "";
+				hmName = "";
+				hmValue = "";
+				tempLenght = 0;
+				startIndex = 0;
+				stopIndex = 0;
+				startIndex = clearCutTail.indexOf(":");
+				tempCut = clearCutTail.substring(0, startIndex);
+				hmName = tempCut;
+				tempLenght = tempCut.length() +1;
+				clearCutTail = clearCutTail.substring(tempLenght, clearCutTail.length());
+				tempCut = "";
+				if (clearCutTail.indexOf("EXPLANATION") != -1)
+				{	
+					stopIndex = clearCutTail.indexOf("EXPLANATION");
+					tempCut = clearCutTail.substring(0, stopIndex);
+					hmValue = tempCut;
+					clearCutTail = clearCutTail.substring(tempCut.length(), clearCutTail.length());
+					tempCut = "";
+					Data.dataList.put(hmName, hmValue);
+				}
+				if (clearCutTail.indexOf("EXPLANATION") == -1)
+				{
+					if (clearCutTail.indexOf("ACTION") != -1)
+					{
+						stopIndex = clearCutTail.indexOf("ACTION");
+						tempCut = clearCutTail.substring(0, stopIndex);
+						hmValue = tempCut;
+						clearCutTail = clearCutTail.substring(tempCut.length(), clearCutTail.length());
+						tempCut = "";
+						Data.dataList.put(hmName, hmValue);
+					}
+					if (clearCutTail.indexOf("ACTION") == -1)
+					{
+						tempCut = clearCutTail.substring(0, clearCutTail.length());
+						hmValue = tempCut;
+						tempCut = "";
+						Data.dataList.put(hmName, hmValue);
+					}
+				}
 		}
 	}
 }
