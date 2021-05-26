11f7bddf798adb2b91d586d42b6c9d5f883d4bc6
Dmitrii Sukharev <sdm831@gmail.com>
2021-05-26 11:53:37 +0300

    export dataList by FileWriter (ver 1)

--- src/GitParse/Main.java
+++ src/GitParse/Main.java
@@ -2,45 +2,40 @@ package GitParse;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Map.Entry;
 
 import static GitParse.Data.dataList;
 
-public class Main 
-{
+public class Main {
 	public static String str;
-	public static void main(String[] args) 
-	{
+
+	public static void main(String[] args) {
 		System.out.println("-------------------------------");
 		System.out.println("Hello, start parse!");
-		try 
-		{
-		File file = new File("test-log-1.txt");
-		//File file = new File("D:\\tmp\\parse_mq\\AMQERR01_1.LOG");
-		FileReader fr = new FileReader(file);
-		BufferedReader br = new BufferedReader(fr);
-		str = br.readLine();
-		String tmpStr = "";
-		while (str != null) 
-		{
-			tmpStr = tmpStr + str;
-			if(str.startsWith("-----")) 
-			{
-				Parse.parse(tmpStr);
-				tmpStr = "";
-			}
+		try {
+			File file = new File("test-log-1.txt");
+			//File file = new File("D:\\tmp\\parse_mq\\AMQERR01_1.LOG");
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
 			str = br.readLine();
-		}
-		br.close();
-		}catch (Exception ex) 
-		{
+			String tmpStr = "";
+			while (str != null) {
+				tmpStr = tmpStr + str;
+				if (str.startsWith("-----")) {
+					Parse.parse(tmpStr);
+					tmpStr = "";
+				}
+				str = br.readLine();
+			}
+			br.close();
+		} catch (Exception ex) {
 			System.out.println(ex);
 		}
 
-		System.out.println("Блок лога: " +Data.str_all);
-		System.out.println("Шапка: " +Data.str_head);
-		System.out.println("Остальное: " +Data.str_tail);
+		System.out.println("Блок лога: " + Data.str_all);
+		System.out.println("Шапка: " + Data.str_head);
+		System.out.println("Остальное: " + Data.str_tail);
 		System.out.println(" ");
 		System.out.println("Print DataList:");
 		//System.out.println(Data.dataList);		// такой вывод не информативен, мешанина какая то получается, только путает
@@ -48,9 +43,22 @@ public class Main
 		// более наглядный вывод "словаря", по образу .json:  key:value, т.е. словарь у тебя собрался как надо
 		for (Entry<String, String> entry : dataList.entrySet()) {
 			System.out.println(entry.getKey() + ":" + entry.getValue());
+
+			try (FileWriter fr = new FileWriter("export.txt", true)) {
+
+				fr.write("\"" + entry.getKey() + "\"" + ":" + "\"" + entry.getValue() + "\"" + ",");
+
+			} catch (IOException ex) {
+
+				System.out.println(ex.getMessage());
+			}
 		}
 
-		System.out.println("-------------------------------");
-
-	} 
+		try (FileWriter fr = new FileWriter("export.txt", true)) {
+			fr.write("\n");
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
 }
+
